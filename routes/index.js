e74320550be7183538513ce10fb8f5592173f157
Tien Pham <r3dsky02@yahoo.com.vn>
2017-11-18 12:53:20 +0200

    Added registration form template

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-// GET register
-router.get('/register', function(req,res,next) {
-  return res.send("Register today!")
+// GET /register
+router.get("/register", function(req,res,next) {
+  return res.render("register", {title: "Sign up"})
+})
+
+// POST /register
+router.post("/register", function(req,res,next){
+  return res.send("User created")
 })
 
 // GET /
-router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+router.get("/", function(req, res, next) {
+  return res.render("index", { title: "Home" });
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
-  return res.render('about', { title: 'About' });
+router.get("/about", function(req, res, next) {
+  return res.render("about", { title: "About" });
 });
 
 // GET /contact
-router.get('/contact', function(req, res, next) {
-  return res.render('contact', { title: 'Contact' });
+router.get("/contact", function(req, res, next) {
+  return res.render("contact", { title: "Contact" });
 });
 
 module.exports = router;
