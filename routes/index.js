27e781af5b32674ac6b83afcb5a0c26285ef8387
Tien Pham <r3dsky02@yahoo.com.vn>
2017-11-18 22:27:19 +0200

    Added error handling

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var User = require("../models/user")
 
 // GET /register
 router.get("/register", function(req,res,next) {
@@ -8,7 +9,23 @@ router.get("/register", function(req,res,next) {
 
 // POST /register
 router.post("/register", function(req,res,next){
-  return res.send("User created")
+  if(req.body.email &&
+    req.body.name &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword){
+
+      //confirm user typed the same password twice
+      if(req.body.password !== req.body.confirmPassword){
+        var err = new Error("Password do not match")
+        err.status = 400
+        return next(err)
+      }
+    }else{
+      var err = new Error("All fields required")
+      err.status = 400
+      return next(err)
+    }
 })
 
 // GET /
