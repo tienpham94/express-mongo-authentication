205c3da7106dffdf249bf297a0eca7dcb2b7a087
Tien Pham <r3dsky02@yahoo.com.vn>
2017-11-19 16:09:27 +0200

    Added login route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+// GET /login
+router.get('/login',function (req,res,next) {
+  return res.render("login", {title: "Log in"})
+})
+
+// post /login
+router.post('/login',function (req,res,next) {
+  return res.render("login", {title: "Logged in!"})
+})
+
 // GET /register
 router.get('/register', function(req, res, next) {
   return res.render('register', { title: 'Sign Up' });
