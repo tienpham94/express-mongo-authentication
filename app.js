6de28b0b78655077ea3512ac41a5f466bc226128
Tien Pham <r3dsky02@yahoo.com.vn>
2017-11-19 17:18:12 +0200

    Working authentication system

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose")
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
 var app = express();
 
-//mongodb connection
-mongoose.connect("mongodb://localhost:27017/bookworm")
-var db = mongoose.connection;
+// use sessions for tracking logins
+app.use(session({
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false
+}));
 
-//mongo Error
-db.on("error", console.error.bind(console, "connection error"))
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/bookworm");
+var db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // serve static files from /public
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 // view engine setup
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
 // include routes
-var routes = require("./routes/index");
-app.use("/", routes);
+var routes = require('./routes/index');
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("File Not Found");
+  var err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
@@ -36,7 +43,7 @@ app.use(function(req, res, next) {
 // define as the last app.use callback
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render("error", {
+  res.render('error', {
     message: err.message,
     error: {}
   });
@@ -44,5 +51,5 @@ app.use(function(err, req, res, next) {
 
 // listen on port 3000
 app.listen(3000, function () {
-  console.log("Express app listening on port 3000");
+  console.log('Express app listening on port 3000');
 });
--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,47 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-// GET /login
-router.get('/login',function (req,res,next) {
-  return res.render("login", {title: "Log in"})
-})
-
-// post /login
-router.post('/login',function (req,res,next) {
-  if(req.body.email && req.body.password){
-    User.authenticate(req.body.email, req.body.password, function(error, user){
-      if (error || !user){
-        var err = new Error("Wrong email or password.")
-        err.status = 401
-        return next(err)
-      }else{
-        req.session.userId = user._id
-        return res.redirect("/profile")
-      }
-    })
-  }else{
-    var err = new Error("Email and password are required")
-    err.status = 401
-    return next(err)
+// GET /profile
+router.get('/profile', function(req, res, next) {
+  if (! req.session.userId ) {
+    var err = new Error("You are not authorized to view this page.");
+    err.status = 403;
+    return next(err);
   }
-})
+  User.findById(req.session.userId)
+      .exec(function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook });
+        }
+      });
+});
+
+// GET /login
+router.get('/login', function(req, res, next) {
+  return res.render('login', { title: 'Log In'});
+});
+
+// POST /login
+router.post('/login', function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      }  else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
 
 // GET /register
 router.get('/register', function(req, res, next) {
@@ -60,7 +77,7 @@ router.post('/register', function(req, res, next) {
         if (error) {
           return next(error);
         } else {
-          req.session.userId = user._id
+          req.session.userId = user._id;
           return res.redirect('/profile');
         }
       });
